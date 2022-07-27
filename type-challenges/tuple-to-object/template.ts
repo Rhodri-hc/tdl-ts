c255dfdacdaa5cb2720164a1286213ad3d6ca331
Rhodri-hc <13622450494@163.com>
2022-07-27 23:40:30 +0800

    easy-tuple-to-object

---
+++ type-challenges/tuple-to-object/template.ts
@@ -0,0 +1,8 @@
+// type TupleToObject<T extends readonly any[]> = any
+
+type TupleToObject<T extends readonly (string | number | symbol)[]> = {
+    [P in T[number]] : P
+}
+
+// 1. 返回一个对象
+// 2. 遍历 array  T[number]
